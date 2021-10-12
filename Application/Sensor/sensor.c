d0044f9682c681a1f81ac79b2660d81070e6b9b9
Ray Ozzie <ray@ozzie.net>
2021-10-12 09:54:21 -0400

    add integration configuration instructions

--- Application/Sensor/sensor.c
+++ Application/Sensor/sensor.c
@@ -4,27 +4,38 @@
 
 #include "sensor.h"
 
-// Measure sensor values and retrieve data to be transmitted, returning
-// true if success or false if failure.
+// Measure sensor values and retrieve data to be transmitted, returning true if success or false if failure.
 bool sensorGetData(uint8_t *buf, uint8_t bufmaxlen, uint8_t *buflen, uint32_t *secsSuccess, uint32_t *secsFailure)
 {
 
-    // Set timings for success and failure
+    // Set callback timings on either success or failure to send this sensor measurement
     *secsFailure = 10;
     *secsSuccess = 15;
 
-    // Append simulated sensor data, matching the template
-    // {"a":11,"b":12,"c":13,"d":14,"temp":12.1,"lat":14.1,"lon":14.1}
+    // Append simulated sensor data, carefully noting that the code must match the template supplied
+    // when configuring the integration in either ttn or helium as follows.  (Note that there is also
+    // an X-Hub header field which can be set to an alternate notehub, which defaults to: api.notefile.net
+    // For template format see:
+    // https://dev.blues.io/notecard/notecard-walkthrough/low-bandwidth-design/#understanding-template-data-types
+    //
+    // Integration Configuration within The Things Network or Helium console
+    //   https://notecard.live/lorawan
+	//   X-Session-Token: <your project's notehub API session token>
+	//   X-Format: ttn <or helium>
+	//   X-Product: <any product UID within your project>
+	//   X-Template: {"a":11,"b":12,"c":13,"d":14,"temp":12.1,"lat":14.1,"lon":14.1}
+    //
     binBegin(buf, bufmaxlen, 0);
-    binAppendInt8(0);
-    binAppendInt16(1);
-    binAppendInt24(2);
-    binAppendInt32(3);
-    binAppendReal16(-1.23);
-    binAppendReal32(4.56);
-    binAppendReal32(7.89);
+    binAppendInt8(0);               // "a"
+    binAppendInt16(1);              // "b"
+    binAppendInt24(2);              // "c"
+    binAppendInt32(3);              // "d"
+    binAppendReal16(-1.23);         // "temp"
+    binAppendReal32(4.56);          // "lat"
+    binAppendReal32(7.89);          // "lon"
     *buflen = binEnd();
 
+    // Done
     APP_PRINTF("sensor retrieved %d bytes\r\n", *buflen);
     return true;
 
